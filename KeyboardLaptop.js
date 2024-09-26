9141f60c2cac120ffb909072f1afd84e5193cf06
KelvinLihandy <kelvinlihandy2005@gmail.com>
2024-09-26 23:32:11 +0700

    26/09

---
+++ KeyboardLaptop.js
@@ -0,0 +1,22 @@
+function pressOn(event){
+    const press = event.code;
+    if(press){
+        const element = document.getElementById(press);
+        element.style.backgroundColor = "White";
+        element.style.color = "Black";
+        element.style.border = "1px solid black"
+    }
+}
+
+function pressOff(event){
+    const press = event.code;
+    if(press){
+        const element = document.getElementById(press);
+        element.style.backgroundColor = "Black";
+        element.style.color = "#bbbaba";
+        element.style.border = "1px solid black"
+    }
+}
+
+document.addEventListener("keydown", pressOn);
+document.addEventListener("keyup", pressOff);
